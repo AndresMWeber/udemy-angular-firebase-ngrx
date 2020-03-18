39d63438a7d012820e26b2590f87e15503dba15e
Andres Weber <andresmweber@gmail.com>
2020-03-18 17:34:27 -0400

    Added environment.ts after reading https://javebratt.com/hide-firebase-api/

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyB0_vhLWEE-dyahRr_E_SE3ydFjy_uz4Rw',
+        authDomain: 'udemy-angular-firebase-ngrx.firebaseapp.com',
+        databaseURL: 'https://udemy-angular-firebase-ngrx.firebaseio.com',
+        projectId: 'udemy-angular-firebase-ngrx',
+        storageBucket: 'udemy-angular-firebase-ngrx.appspot.com',
+        messagingSenderId: '837666564509',
+        appId: '1:837666564509:web:6ba7a92a56fc0be28ae774',
+        measurementId: 'G-K9KRGLNXDP'
+    }
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
